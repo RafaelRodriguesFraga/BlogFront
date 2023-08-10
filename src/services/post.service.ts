cfdaf971e5912a22f823ffe22d7afeec6e77ff71
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-08-10 13:17:40 -0300

    Add GetAllByTag function

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -15,6 +15,22 @@ export const getAll = async (page: number, quantityPerPage: number) => {
   }
 };
 
+export const getAllByTag = async (page: number, quantityPerPage: number, tag: string) => {
+  try {
+    const endpoint = `post/tag/${tag}?currentPage=${page}&quantityPerPage=${quantityPerPage}`;
+
+    const response = await axiosGet(endpoint);
+
+    return response;
+  } catch (error: any) {
+    const { response } = error;
+    if (response.data) {
+      return response.data;
+    }
+    return error;
+  }
+};
+
 export const getBySlug = async (slug: string) => {
   try {
     const endpoint = `post/${slug}`;
@@ -30,3 +46,5 @@ export const getBySlug = async (slug: string) => {
     return error;
   }
 };
+
+
