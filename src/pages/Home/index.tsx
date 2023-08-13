8a093e12a703e146f09e9b08233a985b21c41f45
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-08-13 20:48:21 -0300

    Add SearchResult

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,11 +6,20 @@ import PostPaginated from "../../interfaces/PostPaginated";
 import { getAll } from "../../services/post.service";
 import { format } from "date-fns";
 import Post from "../../interfaces/Post";
+import { useLocation, useNavigate } from "react-router-dom";
+import SearchResult from "../SearchResult";
 
 let page = 1;
 let quantityPerPage = 10;
 
-const Home = () => {
+type HomeProps = {
+  searchResults: Post[];
+};
+const Home = ({ searchResults }: HomeProps) => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const title = queryParams.get("title");
+  console.log(title);
   const [posts, setPosts] = useState<Post[]>([]);
   const [featuredPost, setFeaturedPost] = useState<Post>();
   const [currentPage, setCurrentPage] = useState(1);
@@ -47,10 +56,17 @@ const Home = () => {
   return (
     <S.Container>
       <S.PostContainer>
-        {posts &&
+        {title ? (
+          <SearchResult posts={searchResults} />
+        ) : (
           posts.map((post) => {
             return <PostCard key={post.id} post={post} />;
-          })}
+          })
+        )}
+        {/* {posts &&
+          posts.map((post) => {
+            return <PostCard key={post.id} post={post} />;
+          })} */}
       </S.PostContainer>
 
       <Pagination
