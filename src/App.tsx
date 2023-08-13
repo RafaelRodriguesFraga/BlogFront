28db21a7558106b5a57c2ba7b37fbf4168972541
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-08-13 19:44:49 -0300

    Change Routes to a GlobalLayout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import { Routes } from "./routes/routes";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import GlobalLayout from "./GlobalLayout";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as AppRoutes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import { SearchTermProvider } from "./contexts/SearchTermContext";
 
 function App() {
   return (
     <div className="App">
       <div className="max-width-container">
-        <Routes />
+        <Router>
+          <SearchTermProvider>
+            <GlobalLayout />
+          </SearchTermProvider>
+        </Router>
       </div>
     </div>
   );
