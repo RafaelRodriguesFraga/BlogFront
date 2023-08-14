6c2b2e6fd3a3d8c2e2673fb63c6ba40fe4e99e81
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-08-14 12:49:27 -0300

    Add enviroment variable to axios baseURL

--- src/shared/axiosFunctions.ts
+++ src/shared/axiosFunctions.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5090/api/',
+  baseURL: `${process.env.REACT_APP_API_URL}`,
 });
 
 export const axiosGet = async (endpoint: string) => {
