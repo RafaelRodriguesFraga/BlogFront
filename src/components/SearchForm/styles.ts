fe517c29097626476616cb301295962a23163587
Rafael Fraga <ti.rafaelfraga@gmail.com>
2023-08-15 10:45:57 -0300

    Add background on SearchInput

--- src/components/SearchForm/styles.ts
+++ src/components/SearchForm/styles.ts
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
 export const SearchContainer = styled.form`
+ 
   @media (max-width: 768px) {
     display: none;
   }
 `;
 
 export const SearchInput = styled.input`
-  border-radius: 10px;
-  height: 1.75rem;
+  border-radius: 100px;
+  height: 2rem;
   padding: 10px;
   outline: none;
+  background: rgba(31, 41, 55, 0.60); 
+  border: none;
+  color: white;
 `;
